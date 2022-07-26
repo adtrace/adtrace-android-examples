8f619dd237aea08cafabff847b64d049b949d93f
nasser amini <na3ramini.73@gmail.com>
2022-07-26 16:44:27 +0430

    reatributionviadeeplink: handling deeplink added

--- re-attribution-via-deeplink/src/main/java/amini/test/re_attribution_via_deeplink/MainActivity.java
+++ re-attribution-via-deeplink/src/main/java/amini/test/re_attribution_via_deeplink/MainActivity.java
@@ -1,6 +1,9 @@
 package amini.test.re_attribution_via_deeplink;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -12,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = findViewById(R.id.tv);
+
+
+
+        Intent intent;
+        if(getIntent()!=null){
+            intent = getIntent();
+            if(intent.getData()!=null){
+                Uri uri = intent.getData();
+                handleDeeplink(uri);
+            }
+        }
+
+
     }
+
+    private void handleDeeplink(Uri uri) {
+        // handling deeplink
+    }
+
 }
