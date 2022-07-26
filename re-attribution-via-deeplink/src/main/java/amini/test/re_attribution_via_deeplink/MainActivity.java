0ce679d5484d44f11c658ef9a484b74254ef1c34
nasser amini <na3ramini.73@gmail.com>
2022-07-26 16:38:43 +0430

    reatributionviadeeplink: textview added to screen

--- re-attribution-via-deeplink/src/main/java/amini/test/re_attribution_via_deeplink/MainActivity.java
+++ re-attribution-via-deeplink/src/main/java/amini/test/re_attribution_via_deeplink/MainActivity.java
@@ -2,12 +2,15 @@ package amini.test.re_attribution_via_deeplink;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.tv);
     }
 }
