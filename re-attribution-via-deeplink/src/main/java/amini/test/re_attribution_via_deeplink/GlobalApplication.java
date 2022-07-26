5c96629d813e2a67f5bc5873f6aea03fb7735f9f
nasser amini <na3ramini.73@gmail.com>
2022-07-26 16:28:38 +0430

    reatributionviadeeplink: global application class added

---
+++ re-attribution-via-deeplink/src/main/java/amini/test/re_attribution_via_deeplink/GlobalApplication.java
@@ -0,0 +1,11 @@
+package amini.test.re_attribution_via_deeplink;
+
+import android.app.Application;
+
+public class GlobalApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
